b481491ff404552f08b82bb74e2488c84d6287ed
Paulsen18 <44356837+Paulsen18@users.noreply.github.com>
2019-05-27 10:44:24 +0200

    Function for ny tavle 0.1

--- GruppeFem/js/boards.js
+++ GruppeFem/js/boards.js
@@ -1,12 +1,21 @@
 board = {
     board1:{
         name: "Webprosjekt",
-        lists: [list.list1, list.list2, list.list3],
+        lists: [],
         member: [],
     }
 };
 
+function leggTilTavle(){
+    var row = document.getElementById("Group");
+    var x = row.insertCell(-1);
+    var y = document.getElementsByTagName("td").length;
+    x.innerHTML = "tavle  " + y;
+    x.onClick = addGroup();
+}
 
-//lists.append(list.list4);
-
-//console.log(board.board1.lists[0].cards[0].name);
+function addGroup()
+{
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+}
