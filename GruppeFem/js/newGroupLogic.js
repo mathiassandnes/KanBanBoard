5fd25a42ea25d8f51af66d8b7a087686b027373f
Asadprog <rehman19942008@hotmail.com>
2019-06-03 17:08:58 +0200

    Fix some bugs

--- GruppeFem/js/newGroupLogic.js
+++ GruppeFem/js/newGroupLogic.js
@@ -2,18 +2,18 @@
 
 
 
-let names = [];
+let usernames = [];
 let members = [];
 
 
 // gjør at names blir et array av alle navn i databasen
 for (let i = 0; i < users.length; i++) {
-  names.push(users[i].name);
+  usernames.push(users[i].username);
 
 }
 
 //alfabetisk rekkefølge
-names.sort();
+usernames.sort();
 
 ul = document.getElementById("search-result");
 ul.style = "list-style: none; padding: 0;"
@@ -28,14 +28,14 @@ var render_lists = function (lists) {
   ul.innerHTML = li;
 }
 
-render_lists(names);
+render_lists(usernames);
 
 // lets filters it
 input = document.getElementById('user-search-input');
 
 var filterUsers = function (event) {
   keyword = input.value.toLowerCase();
-  filtered_users = names.filter(function (user) {
+  filtered_users = usernames.filter(function (user) {
     user = user.toLowerCase();
     return user.indexOf(keyword) > -1;
   });
@@ -47,29 +47,20 @@ input.addEventListener('keyup', filterUsers);
 
 
 
-
+1
 //trykker man på et navn, legges de til i listen
 
-function addClickInput(name) {
+function addClickInput(username) {
   // 1. legges til i members liste
 
-  members.push(name);
+  members.push(username);
 
   let membersString = '';
 
   for (let x = 0; x < members.length; x++) {
 
-    membersString += '<button class= btn-primary id = x>' + members[x] + '</button>';
-    membersString += '<button class= btn-primary id = x  onclick= removeClickInput(this.id) >' + "X" + '</button>';
-  
-    
-
-
-
-
-
-
-
+     membersString += '<button class= btn-primary id = btn' + x + ' onclick = removeClickInput(' + x + ') >' + members[x] + '</button>';
+     membersString += '<button class= btn-primary id = xbtn' + x + ' onclick= removeClickInput(' + x + ') >' + "X" + '</button>';
   }
 
   document.getElementById("members").innerHTML = membersString;
@@ -85,22 +76,21 @@ function addClickInput(name) {
 
 
 function removeClickInput(id) {
+//  for (let j = 0; j < members.length; j++) {
+ //   if (document.getElementById(x) === members[j].username)
+//   members.splice(j,1);
+  //  console.log(members);
+ // }
 
-  let removeButton = document.getElementById(id );
+  let removeButton = document.getElementById('btn' + id);
   removeButton.parentNode.removeChild(removeButton);
-  members.splice(id, 1);
+  let removeXButton = document.getElementById('xbtn' + id );
+  removeXButton.parentNode.removeChild(removeXButton);
 
-
+  console.log(members);
+  
 
 }
-
-
-
-
-
-
-
-
 
 
 
--- GruppeFem/js/users.js
+++ GruppeFem/js/users.js
@@ -2,194 +2,117 @@ var users = [
     {
         name: "Arne",
         epost: "kek@kek.kek1",
-        username: "kek1",
+        username: "kek3453451",
         password: "kek1"
     },
     {
         name: "Arnulf",
         epost: "kek@kek.kek2",
-        username: "kek12",
+        username:"kek1345345342",
         password: "kek12"
     },
     {
         name: "Bengt",
         epost: "kek@kek.kek1",
-        username: "kek1",
+        username: "kek157847543345",
         password: "kek1"
     },
     {
         name: "Tomas",
         epost: "kek@kek.kek2",
-        username: "kek12",
+        username: "kek156666772",
         password: "kek12"
     },
     {
         name: "Andreas",
         epost: "kek@kek.kek3",
-        username: "kek123",
+        username: "kek16534654323",
         password: "kek123"
     },
     {
         name: "Michael",
         epost: "kek@kek.kek1",
-        username: "kek1",
+        username: "kek6664361",
         password: "kek1"
     },
     {
         name: "Rolando",
         epost: "kek@kek.kek2",
-        username: "kek12",
+        username: "kek1657572",
         password: "kek12"
     },
     {
         name: "Sturla",
         epost: "kek@kek.kek3",
-        username: "kek123",
+        username: "kek157576586523",
         password: "kek123"
     },
     {
         name: "Johannes",
         epost: "kek@kek.kek1",
-        username: "kek1",
+        username: "kek7777464q31",
         password: "kek1"
     },
     {
         name: "Henrik",
         epost: "kek@kek.kek2",
-        username: "kek12",
+        username: "kek5657657812",
         password: "kek12"
     },
     {
         name: "Tor",
         epost: "kek@kek.kek3",
-        username: "kek123",
+        username: "kek165465723",
         password: "kek123"
     },
     {
         name: "Arne",
         epost: "kek@kek.kek1",
-        username: "kek1",
+        username: "kek65751",
         password: "kek1"
     },
     {
         name: "Arnulf",
         epost: "kek@kek.kek2",
-        username: "kek12",
+        username: "kek18982",
         password: "kek12"
     },
     {
         name: "Asad",
         epost: "kek@kek.kek3",
-        username: "kek123",
+        username: "kek127976863",
         password: "kek123"
     },
     {
         name: "Asshole",
         epost: "kek@kek.kek1",
-        username: "kek1",
+        username: "ke7867k1",
         password: "kek1"
     },
     {
         name: "August",
         epost: "kek@kek.kek2",
-        username: "kek12",
+        username: "kek10000002",
         password: "kek12"
     },
     {
         name: "Angelika",
         epost: "kek@kek.kek3",
-        username: "kek123",
+        username: "kek165544323",
         password: "kek123"
     },
     {
         name: "Anne",
         epost: "kek@kek.kek1",
-        username: "kek1",
+        username: "kek9999991",
         password: "kek1"
     },
     {
         name: "Mathias",
         epost: "kek@kek.kek2",
-        username: "kek12",
+        username: "kek1897755432",
         password: "kek12"
     },
-    {
-        name: "Peter",
-        epost: "kek@kek.kek3",
-        username: "kek123",
-        password: "kek123"
-    },
-    {
-        name: "Markus",
-        epost: "kek@kek.kek1",
-        username: "kek1",
-        password: "kek1"
-    },
-    {
-        name: "Kjetil",
-        epost: "kek@kek.kek2",
-        username: "kek12",
-        password: "kek12"
-    },
-    {
-        name: "Jensa",
-        epost: "kek@kek.kek3",
-        username: "kek123",
-        password: "kek123"
-    },
-    {
-        name: "Bengt",
-        epost: "kek@kek.kek1",
-        username: "kek1",
-        password: "kek1"
-    },
-    {
-        name: "Tomas",
-        epost: "kek@kek.kek2",
-        username: "kek12",
-        password: "kek12"
-    },
-    {
-        name: "Andreas",
-        epost: "kek@kek.kek3",
-        username: "kek123",
-        password: "kek123"
-    },
-    {
-        name: "Michael",
-        epost: "kek@kek.kek1",
-        username: "kek1",
-        password: "kek1"
-    },
-    {
-        name: "Rolando",
-        epost: "kek@kek.kek2",
-        username: "kek12",
-        password: "kek12"
-    },
-    {
-        name: "Sturla",
-        epost: "kek@kek.kek3",
-        username: "kek123",
-        password: "kek123"
-    },
-    {
-        name: "Johannes",
-        epost: "kek@kek.kek1",
-        username: "kek1",
-        password: "kek1"
-    },
-    {
-        name: "Henrik",
-        epost: "kek@kek.kek2",
-        username: "kek12",
-        password: "kek12"
-    },
-    {
-        name: "Tor",
-        epost: "kek@kek.kek3",
-        username: "kek123",
-        password: "kek123"
-    },
+ 
 
 ];
