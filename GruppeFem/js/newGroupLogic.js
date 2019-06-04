67f9955d47297cb50097b7777d06e0aae8052ecd
Asadprog <rehman19942008@hotmail.com>
2019-06-04 15:14:23 +0200

    update

--- GruppeFem/js/newGroupLogic.js
+++ GruppeFem/js/newGroupLogic.js
@@ -1,13 +1,14 @@
 
-let usernames = [];
+let names = [];
 let members = [];
+
 
 // gjør at names blir et array av alle navn i databasen
 for (let i = 0; i < users.length; i++) {
-  usernames.push(users[i].username);
+  names.push(users[i].name);
 }
 //alfabetisk rekkefølge
-usernames.sort();
+names.sort();
 
 ul = document.getElementById("search-result");
 ul.style = "list-style: none; padding: 0;"
@@ -19,14 +20,14 @@ var render_lists = function (lists) {
   }
   ul.innerHTML = li;
 }
-render_lists(usernames);
+render_lists(names);
 
 // lets filters it
 input = document.getElementById('user-search-input');
 
 var filterUsers = function (event) {
   keyword = input.value.toLowerCase();
-  filtered_users = usernames.filter(function (user) {
+  filtered_users = names.filter(function (user) {
     user = user.toLowerCase();
     return user.indexOf(keyword) > -1;
   });
@@ -38,29 +39,37 @@ input.addEventListener('keyup', filterUsers);
 
 //trykker man på et navn, legges de til i listen
 
-function addClickInput(username) {
+function addClickInput(name) {
   // 1. legges til i members liste
 
-  members.push(username);
+  members.push(name);
 
   let membersString = '';
 
   for (let x = 0; x < members.length; x++) {
-
-     membersString += '<button class= btn-primary id = btn' + x + ' onclick = removeClickInput(' + x + ') >' + members[x] + '</button>';
-     membersString += '<button class= btn-primary id = xbtn' + x + ' onclick= removeClickInput(' + x + ') >' + "X" + '</button>';
-  }
+    if(members[x] !== null){
+      membersString += '<button class= btn-primary id = btn' + x + ' onclick = removeClickInput(' + x + ') >' + members[x] + '</button>';
+      membersString += '<button class= btn-primary id = xbtn' + x + ' onclick= removeClickInput(' + x + ') >' + "X" + '</button>';
+    }
+      console.log(members);
+    }
      document.getElementById("members").innerHTML = membersString;
 
      //3. Laste inn listene på nytt til siden
     }
-function removeClickInput(id) {
-  
-  members.pop(id)
+function removeClickInput(id)  {
+  members[id] = null;
   let removeButton = document.getElementById('btn' + id);
   removeButton.parentNode.removeChild(removeButton);
   let removeXButton = document.getElementById('xbtn' + id );
   removeXButton.parentNode.removeChild(removeXButton);
   console.log(members);
+}   
 
+function filterNotNull(name){ 
+  return name !== null;
+}
+
+function membersNotNull(){
+  
 }
